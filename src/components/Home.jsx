099d48c16286a0f309f9bddc444df608355b063c
Ruth Wanjiru Mwaniki <75331672+Ruth960@users.noreply.github.com>
2025-03-13 03:00:14 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,13 @@ import Navbar from "./navBar";
 
 export default function Home() {   
     const navLinks = [
-        {text: "'Home', path: '/home'"},
-        {text: "'About', path: '/about'"},
-        {text: "'Contact', path: '/contact'"},
-        {text: "Services', path: '/services'"},
-        {text: "'Projects', path: '/projects'"},
-        {text: "'Blog', path: '/blog'"},
-        {text: "'FAQ' path: '/faq'"},
+        { text: 'Home' },
+        { text: 'About' },
+        { text: 'Contact' },
+        { text: 'Services' },
+        { text: 'Projects' },
+        { text: 'Blog' },
+        { text: 'FAQ' },
 
     ];
     return (
@@ -25,7 +25,7 @@ export default function Home() {
                     <h1>Ruth Wanjiru</h1>
                     <p>Hi there! I'm Ruth, a tech enthusiast with a Bachelor degree in in Internet of Things. I love exploring innovative solutions through technology and have worked on exciting projects ranging from home automation to AI-driven smart assistants. I'm passionate about creating impactful solutions, whether it's through data analytics, digitalization, or cutting-edge technology.
                     </p>
-                    <button style={{ backgroundColor: '#130614', color: 'white',  borderRadius: '5px', width:'100px', hieght:'20px' }}>About Me</button>
+                    <button style={{ backgroundColor: '#130614', color: 'white',  borderRadius: '5px', width:'100px', hieght:'60px',marginTop:'30px' }}>About Me</button>
                 </div>
                 <div>
                     <p>.</p>
